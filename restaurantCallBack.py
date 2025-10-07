9e230c7d5e33766591fc52c51d66fdedf6812e49
Sandro DS <SandroDS@outlook.com>
2025-10-07 13:14:37 +0200

    Callback bool

---
+++ restaurantCallBack.py
@@ -0,0 +1,19 @@
+#it is a callBack example. customer in restaurant put order and waiter deliver food
+def checkFoodExistence(food_name:str)->bool:
+    arr_foods=["Pizza", "Pasta", "Döner"]
+    if food_name in arr_foods:
+        return True
+    else:
+        return False
+
+def orderFood(food_name:str, checkFoodExistence):
+    is_food_exists = checkFoodExistence(food_name)
+    if is_food_exists == True:
+        print(f"your {food_name} deliver soon!")
+    else:
+        print(f"sorry, we have no such food called {food_name}")
+    checkFoodExistence(food_name)
+
+#-- actual program use upper defined methods --#
+order_string = input("what is your order?")
+orderFood(order_string,checkFoodExistence)
